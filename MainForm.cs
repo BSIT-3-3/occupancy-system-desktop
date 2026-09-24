9a36e00d9eebc7a71d6b86942b15254df4b8c190
Elmer <bantoyelmer2@gmail.com>
2026-09-24 23:17:38 +0800

    feat: create mainform but still dont have other components

--- MainForm.cs
+++ MainForm.cs
@@ -7,16 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Krypton.Toolkit;
+using OccupancyMonitor.Forms;
 
 namespace OccupancyMonitor
 {
     public partial class MainForm : Form
     {
+        private prac _prac;
         public MainForm()
         {
             InitializeComponent();
+            _prac = new prac();
+            ShowScreen(_prac);
         }
 
-  
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            kbtnDashboard.StateCommon.Content.ShortText.TextH = Krypton.Toolkit.PaletteRelativeAlign.Center;
+            kbtnDashboard.StateCommon.Content.ShortText.TextV = Krypton.Toolkit.PaletteRelativeAlign.Center;
+            kbtnDashboard.OverrideFocus.Border.Draw = InheritBool.False;
+        }
+
+        private void ShowScreen(UserControl screen)
+        {
+            plContent.Controls.Clear();
+
+            screen.Dock = DockStyle.Fill;
+
+            plContent.Controls.Add(screen);
+        }
+
+        private void kbtnDashboard_Click(object sender, EventArgs e)
+        {
+            ShowScreen(_prac);
+        }
     }
 }
